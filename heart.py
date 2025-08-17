3b94deb54adadabfb94f62d3308e5c717204dfca
Yahia Hamed <yahiamh309090@outlook.com>
2025-08-17 21:56:56 +0300

    crazy

---
+++ heart.py
@@ -0,0 +1,17 @@
+import turtle
+
+t= turtle.Turtle()
+s= turtle.Screen()
+s.bgcolor("black")
+t.fillcolor("red")
+t.begin_fill()
+t.left(50)
+t.fd(120)
+t.circle(45,200)
+t.lt(221)
+t.circle(45,200)
+t.fd(130)
+t.end_fill()
+turtle.done()
+t.hideturtle()
+
